255f5037e00f04d4272ffd8cd8b43d3ca9750007
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-10-22 14:03:32 +0600

    navbar started

---
+++ app/welcome/page.tsx
@@ -0,0 +1,12 @@
+import NavbarWc from "@/components/NavbarWc";
+import React from "react";
+
+function page() {
+  return (
+    <div className="min-h-screen bg-gray-800">
+      <NavbarWc></NavbarWc>
+    </div>
+  );
+}
+
+export default page;
---
+++ components/NavbarWc.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavbarWc() {
+  return (
+    <div className="h-20 shadow-2xl bg-gray-700 text-white flex items-center justify-between px-5">
+      <div>Logo</div>
+      <div>Sign In</div>
+    </div>
+  );
+}
+
+export default NavbarWc;
